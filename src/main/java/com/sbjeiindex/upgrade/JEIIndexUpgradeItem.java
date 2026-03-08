32298a6fcbd3b2173ee397910334ede18576f73a
amicbeam <1425257712@qq.com>
2026-03-08 22:46:57 +0800

    fix: forge config

--- src/main/java/com/sbjeiindex/upgrade/JEIIndexUpgradeItem.java
+++ src/main/java/com/sbjeiindex/upgrade/JEIIndexUpgradeItem.java
@@ -5,15 +5,32 @@ import net.p3pp3rf1y.sophisticatedcore.api.IStorageWrapper;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeItemBase;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeType;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeWrapperBase;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.IUpgradeCountLimitConfig;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeGroup;
+import net.minecraft.resources.ResourceLocation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.function.Consumer;
 
 public class JEIIndexUpgradeItem extends UpgradeItemBase<JEIIndexUpgradeItem.Wrapper> {
     public static final UpgradeType<Wrapper> TYPE = new UpgradeType<>(Wrapper::new);
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public JEIIndexUpgradeItem() {
-        super(1);
+        super(new IUpgradeCountLimitConfig() {
+            @Override
+            public int getMaxUpgradesInGroupPerStorage(String storageId, UpgradeGroup group) {
+                return 1;
+            }
+
+            @Override
+            public int getMaxUpgradesPerStorage(String storageId, ResourceLocation upgradeId) {
+                return 1;
+            }
+        });
+        LOGGER.info("JEI Index Upgrade item created");
     }
 
     @Override
@@ -29,6 +46,7 @@ public class JEIIndexUpgradeItem extends UpgradeItemBase<JEIIndexUpgradeItem.Wra
     public static class Wrapper extends UpgradeWrapperBase<Wrapper, JEIIndexUpgradeItem> {
         public Wrapper(IStorageWrapper backpackWrapper, ItemStack upgrade, Consumer<ItemStack> upgradeSaveHandler) {
             super(backpackWrapper, upgrade, upgradeSaveHandler);
+            LOGGER.info("JEI Index Upgrade wrapper created for backpack: {}", backpackWrapper);
         }
 
         @Override
