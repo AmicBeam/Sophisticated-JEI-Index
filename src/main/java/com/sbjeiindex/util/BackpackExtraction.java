df91cc89b094249ea5a186b4a5c5b85bb3c809b6
amicbeam <limengze.2001@bytedance.com>
2026-03-12 15:56:05 +0800

    feat: actually compatible more mods
    Change-Id: Ie5e27c78d2bf460eeba62213b18c81fbb819e8da

--- src/main/java/com/sbjeiindex/util/BackpackExtraction.java
+++ src/main/java/com/sbjeiindex/util/BackpackExtraction.java
@@ -47,5 +47,41 @@ public class BackpackExtraction {
         }
         return ItemStack.EMPTY;
     }
-}
 
+    public static boolean reserveIngredient(InventoryHandler handler, int[] reservedCounts, Ingredient ingredient) {
+        int slots = handler.getSlots();
+        for (int i = 0; i < slots; i++) {
+            ItemStack stack = handler.getStackInSlot(i);
+            if (stack.isEmpty()) {
+                continue;
+            }
+            int available = stack.getCount() - reservedCounts[i];
+            if (available > 0 && ingredient.test(stack)) {
+                reservedCounts[i]++;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean reserveFirstTemplateMatch(InventoryHandler handler, int[] reservedCounts, ItemStack[] templates) {
+        int slots = handler.getSlots();
+        for (int i = 0; i < slots; i++) {
+            ItemStack stack = handler.getStackInSlot(i);
+            if (stack.isEmpty()) {
+                continue;
+            }
+            int available = stack.getCount() - reservedCounts[i];
+            if (available <= 0) {
+                continue;
+            }
+            for (ItemStack template : templates) {
+                if (template != null && !template.isEmpty() && ItemStack.isSameItemSameTags(stack, template)) {
+                    reservedCounts[i]++;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
