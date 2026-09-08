c302040618c7d03d68c15d4834f8a6cc0e14a6df
amicbeam <limengze.2001@bytedance.com>
2026-09-09 00:04:23 +0800

    fix: defer client sync listener registration
    Change-Id: I19d9a552b11f3dad3136d1390122e4cce199a672

--- src/main/java/com/sbjeiindex/SBJEIIndex.java
+++ src/main/java/com/sbjeiindex/SBJEIIndex.java
@@ -2,6 +2,7 @@ package com.sbjeiindex;
 
 import com.sbjeiindex.config.SBJEIIndexConfig;
 import com.sbjeiindex.init.ModItems;
+import com.sbjeiindex.util.ClientBackpackContentsPrimer;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
@@ -10,6 +11,8 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 
 @Mod(SBJEIIndex.MOD_ID)
@@ -22,11 +25,18 @@ public class SBJEIIndex {
         container.registerConfig(ModConfig.Type.COMMON, SBJEIIndexConfig.SPEC);
         ModItems.ITEMS.register(modBus);
         modBus.addListener(this::addCreative);
+        if (dist == Dist.CLIENT) {
+            modBus.addListener(this::clientSetup);
+        }
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == SOPHISTICATED_BACKPACKS_TAB && !SBJEIIndexConfig.enableTransferWithoutUpgrade.get()) {
             event.accept(ModItems.JEI_INDEX_UPGRADE.get());
         }
+    }
+
+    private void clientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> NeoForge.EVENT_BUS.addListener(ClientBackpackContentsPrimer::onClientTick));
     }
 }
--- src/main/java/com/sbjeiindex/util/ClientBackpackContentsPrimer.java
+++ src/main/java/com/sbjeiindex/util/ClientBackpackContentsPrimer.java
@@ -1,22 +1,16 @@
 package com.sbjeiindex.util;
 
-import com.sbjeiindex.SBJEIIndex;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.player.Player;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 
 /** Primes linked backpack snapshots before JEI asks whether a transfer can be performed. */
-@EventBusSubscriber(modid = SBJEIIndex.MOD_ID, value = Dist.CLIENT)
 public final class ClientBackpackContentsPrimer {
     private static final int PRIME_INTERVAL_TICKS = 20;
     private static int ticksUntilPrime;
 
     private ClientBackpackContentsPrimer() {}
 
-    @SubscribeEvent
     public static void onClientTick(ClientTickEvent.Post event) {
         if (ticksUntilPrime > 0) {
             ticksUntilPrime--;
